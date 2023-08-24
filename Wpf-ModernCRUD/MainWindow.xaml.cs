d2179a28bcab4c4931de9270741b0de01758bee4
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-08-24 15:08:10 +0100

    add edit and add function

--- Wpf-ModernCRUD/MainWindow.xaml.cs
+++ Wpf-ModernCRUD/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf_ModernCRUD.EntityModel;
+using Wpf_ModernCRUD.Pages;
 
 namespace Wpf_ModernCRUD
 {
@@ -20,9 +22,62 @@ namespace Wpf_ModernCRUD
     /// </summary>
     public partial class MainWindow : Window
     {
+        ModerbCRUDEntities _db = new ModerbCRUDEntities();
+        public static DataGrid datagrid;
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            DGridCustomer.ItemsSource = _db.CustomerDBs.ToList();
+            datagrid = DGridCustomer;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            int ID = (DGridCustomer.SelectedItem as CustomerDB).ID;
+            EditDataWin editDataWin = new EditDataWin(ID);
+            editDataWin.ShowDialog();
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int ID = (DGridCustomer.SelectedItem as CustomerDB).ID;
+            var deleteCustomer = _db.CustomerDBs.Where(m => m.ID == ID).Single();
+            _db.CustomerDBs.Remove(deleteCustomer);
+            _db.SaveChanges();
+            DGridCustomer.ItemsSource = _db.CustomerDBs.ToList();
+            MessageBox.Show("Delete Data Success!", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddDataWin addDataWin = new AddDataWin();
+            addDataWin.ShowDialog();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var result = _db.CustomerDBs.Where(x => x.Name.Contains(TBoxSearch.Text) || x.Address.Contains(TBoxSearch.Text) || x.Phone.Contains(TBoxSearch.Text)).ToList();
+            DGridCustomer.ItemsSource = result;
         }
     }
 }
+
